25dde46a5d10b23c770e30f56b6724a572b055f8
Joshua M <91457812+JoshuaMicallefYBSU@users.noreply.github.com>
2026-08-14 22:18:30 +1000

    FDR Updates

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Schedule;
 use App\Jobs\SyncVatsysDatasetJob;
 use App\Jobs\AFVTransieversUpdate;
 use App\Jobs\ReleaseStaleSectorOwnershipsJob;
+use App\Jobs\PruneFlightDataRecordsJob;
 
 Schedule::job(new SyncVatsysDatasetJob)->dailyAt('10:15');
 Schedule::job(new AFVTransieversUpdate)->everyMinute()->withoutOverlapping();
 Schedule::job(new ReleaseStaleSectorOwnershipsJob)->everyMinute()->withoutOverlapping();
+Schedule::job(new PruneFlightDataRecordsJob)->everyFiveMinutes();
